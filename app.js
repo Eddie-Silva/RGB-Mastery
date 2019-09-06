740e99afd8ddce6e05890415f33a6b772caae4cf
Eddie Silva <edsildo@gmail.com>
2019-09-06 15:55:27 -0500

    UPDATE to node app and use express

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"))
+
+app.get("/", function(req, res){
+   res.sendFile(path.join(__dirname + "/public/index.html"))
+})
+
+const port = process.env.PORT || 3000;
+const ip = process.env.IP || "0.0.0.0/0";
+app.length(port, function(){
+
+});
